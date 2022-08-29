9f451d4d49b928ee60b90e2996ad7902083d43a6
Nahuel Pazos <rodrigopazos9@gmail.com>
2022-08-29 11:09:08 -0300

    2

---
+++ Ejercicio_14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero;
+            long binario;
+
+            Console.WriteLine("Ingrese un numero:");
+            numeroEntero = int.Parse(Console.ReadLine());
+
+            binario = Class1.ConvertirDecimalABinario(numeroEntero);
+
+            Console.WriteLine("\nEl numero decimal {0} en binario es {1} ",numeroEntero,binario);
+
+        }
+    }
+}
